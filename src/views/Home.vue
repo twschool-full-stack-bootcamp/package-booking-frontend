38a4bd501c9418a0fdb99fc524367d2b11f9f604
DALLIN GUO (ITA-ISDC-ISD-OOCLL/ZHA) <GUODA@OOCL.COM>
2019-07-25 12:02:46 +0800

    design_mainPage_but_not_fetch_data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div class="home">   
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p> 首页</p>
+    <p>
+      菜鸟驿站 
+        <el-button type="info" round  @click="filter_all">All</el-button>
+        <el-button type="info" round  @click="filter_reserved">已预约</el-button>
+        <el-button type="info" round  @click="filter_picked">已经取件</el-button> 
+        <el-button type="info" round  @click="filter_not_reserved">未预约</el-button> 
+        <el-button type="info" round  @click="add">添加</el-button>
+    </p>
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="运单号"
+        width="180">
+      </el-table-column>
+      <el-table-column 
+        prop="name"
+        label="收件人"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address" 
+        label="电话">
+      </el-table-column>
+      <el-table-column
+        prop="date"
+        label="状态"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="date"
+        label="预约时间"
+        width="180">
+      </el-table-column>
+    </el-table>
+
   </div>
 </template>
 
+
+
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Main from '@/components/Main.vue'
+
 
 export default {
-  name: 'home',
+  name: 'main',
   components: {
-    HelloWorld
-  }
+    Main
+  },
+  data: function (){
+       return{
+           
+       } 
+    },
 }
 </script>
